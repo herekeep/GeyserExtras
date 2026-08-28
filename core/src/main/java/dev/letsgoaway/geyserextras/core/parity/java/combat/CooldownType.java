885642348f929869fa3105e2d17183e55e580949
herekeep <3837037426@qq.com>
2026-08-28 17:32:20 +0800

    添加 getCooldownHandler() 方法到 ExtrasPlayer

---
+++ core/src/main/java/dev/letsgoaway/geyserextras/core/parity/java/combat/CooldownType.java
@@ -0,0 +1,7 @@
+package dev.letsgoaway.geyserextras.core.parity.java.combat;
+
+public enum CooldownType {
+    OFF,
+    CROSSHAIR,
+    HOTBAR
+}
--- core/src/main/java/dev/letsgoaway/geyserextras/core/parity/java/combat/CooldownUtils.java
+++
@@ -1,7 +0,0 @@
-package dev.letsgoaway.geyserextras.core.parity.java.combat;
-
-public enum CooldownType {
-    OFF,        // 禁用
-    CROSSHAIR,  // 准星下方
-    HOTBAR      // 快捷栏上方
-}
